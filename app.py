fadfcb35a8c391cc31316461f5a8df865b360039
Nishanthini-10 <nishanthini20062511@gmail.com>
2025-08-29 15:07:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+@app.route("/")
 def home():
     return render_template("index.html")
 
@@ -36,4 +36,5 @@ def calculate():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
